110205d23d231abb587d159e5d25350eb20adca8
Sanjay Gandhi <sanj.redfield@gmail.com>
2019-07-25 14:49:51 +0530

    Remove app hash stuff

--- src/android/CordovaPluginAndroidSmsRetriever.java
+++ src/android/CordovaPluginAndroidSmsRetriever.java
@@ -46,11 +46,6 @@ public class CordovaPluginAndroidSmsRetriever extends CordovaPlugin {
             return true;
         }
 
-        if (action.equals("getAppHash")) {
-            this.getAppHash(callbackContext);
-            return true;
-        }
-
         callbackContext.error("Android SMS Retriever Plugin: Unknown action (" + action + ")");
         return false;
     }
@@ -114,71 +109,4 @@ public class CordovaPluginAndroidSmsRetriever extends CordovaPlugin {
         });
 
     }
-
-    private void getAppHash(CallbackContext callbackContext) {
-
-        try {
-
-            for (String hash : getAppSignatures(this.cordova.getActivity().getApplicationContext(), callbackContext)) {
-                callbackContext.success("App's hash: " + hash + " - DO NOT USE THIS UTILITY IN PRODUCTION");
-                return;
-            }
-
-            callbackContext.error(
-                    "The app's hash could not be computed. Please refer to Google documentation for an alternative: https://developers.google.com/identity/sms-retriever/verify#computing_your_apps_hash_string");
-        } catch (Exception e) {
-
-            callbackContext.error("Something bad happened... :( Exception message: " + e.getMessage());
-        }
-    }
-
-    private static final String HASH_TYPE = "SHA-256";
-    public static final int NUM_HASHED_BYTES = 9;
-    public static final int NUM_BASE64_CHAR = 11;
-
-    private ArrayList<String> getAppSignatures(Context context, CallbackContext callbackContext) {
-        ArrayList<String> appCodes = new ArrayList<>();
-
-        try {
-            // Get all package signatures for the current package
-            String packageName = context.getPackageName();
-            PackageManager packageManager = context.getPackageManager();
-            Signature[] signatures = packageManager.getPackageInfo(packageName,
-                    PackageManager.GET_SIGNATURES).signatures;
-
-            // For each signature create a compatible hash
-            for (Signature signature : signatures) {
-                String hash = hash(packageName, signature.toCharsString(), callbackContext);
-                if (hash != null) {
-                    appCodes.add(String.format("%s", hash));
-                }
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            callbackContext.error("Unable to find package to obtain hash.");
-        }
-
-        return appCodes;
-    }
-
-    private static String hash(String packageName, String signature, CallbackContext callbackContext) {
-        String appInfo = packageName + " " + signature;
-        try {
-            MessageDigest messageDigest = MessageDigest.getInstance(HASH_TYPE);
-            messageDigest.update(appInfo.getBytes(StandardCharsets.UTF_8));
-            byte[] hashSignature = messageDigest.digest();
-
-            // truncated into NUM_HASHED_BYTES
-            hashSignature = Arrays.copyOfRange(hashSignature, 0, NUM_HASHED_BYTES);
-            // encode into Base64
-            String base64Hash = Base64.encodeToString(hashSignature, Base64.NO_PADDING | Base64.NO_WRAP);
-            base64Hash = base64Hash.substring(0, NUM_BASE64_CHAR);
-
-            return base64Hash;
-        } catch (NoSuchAlgorithmException e) {
-
-            callbackContext.error("hash:NoSuchAlgorithm");
-        }
-        return null;
-    }
-
 }
--- www/cordova-plugin-android-sms-retriever.js
+++ www/cordova-plugin-android-sms-retriever.js
@@ -3,7 +3,3 @@ var exec = require('cordova/exec');
 exports.onSmsReceived = function(success, error, notifyWhenStarted) {
     exec(success, error, "CordovaPluginAndroidSmsRetriever", "onSmsReceived", [ notifyWhenStarted ]);
 };
-
-exports.getAppHash = function(success, error) {
-    exec(success, error, "CordovaPluginAndroidSmsRetriever", "getAppHash", []);
-};
